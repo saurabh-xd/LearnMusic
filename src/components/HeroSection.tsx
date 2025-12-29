59946d31c5b374cd1d6e16b4f08082202d10a6ff
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-12-29 18:44:21 +0530

    update buuton and subtext in hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -22,12 +22,12 @@ function HeroSection() {
         bg-gradient-to-b from-neutral-50 to-neutral-400">Master the art of music</h1>
         <p 
         className="mt-4 font-normal text-base md:text-lg 
-        text-neutral-300 max-w-lg mx-auto">
+        text-neutral-300/80 max-w-xl mx-auto">
           Dive into our comprehensive music courses and transform your musical
           journey today. Whether you are a beginner or looking to refine your
           skills, join us to unlock your true potential.
         </p>
-        <div className="mt-3">
+        <div className="mt-6">
             <Link href={"/courses"}>
             <Button
         borderRadius="1.75rem"
